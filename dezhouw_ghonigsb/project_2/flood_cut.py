052e61d4c78c53514ea35f23d65cc0d911ff7913
dezhouw <hongsinwang@gmail.com>
2019-05-01 06:42:33 -0400

    .

--- dezhouw_ghonigsb/project_2/flood_cut.py
+++ dezhouw_ghonigsb/project_2/flood_cut.py
@@ -29,7 +29,8 @@ class Flood_Cut:
 		self.area_threshold = 0.8
 		self.wall_threshold = 0.3
 
-		answer = self._find_match_result()
+		self._generate_overlapping_zones()
+		# answer = self._find_match_result()
 
 	######## private functions ########
 
@@ -38,8 +39,8 @@ class Flood_Cut:
 		# list of <class 'shapely.geometry.polygon.Polygon'>
 		flood_collection = self.db['dezhouw_ghonigsb.sea_level']
 		flood_geometry = flood_collection.find()[3]\
-		                 ['thirty_six_inch_sea_level_rise_10_pct_annual_flood']\
-		                 ['features'][0]['geometry']
+										 ['thirty_six_inch_sea_level_rise_10_pct_annual_flood']\
+										 ['features'][0]['geometry']
 		flood = [x.buffer(0) for x in shape(flood_geometry).buffer(0).geoms]
 		self.flood = MultiPolygon(flood)
 
@@ -58,21 +59,41 @@ class Flood_Cut:
 
 	def _generate_overlapping_zones(self) -> None:
 		# 408 different zones overlapping with the flood area
+		# Insert these result data into MongoDB
+		# overlapping_zones = self.db['overlapping_zones']
+		# overlapping_zones.delete_many({})
 		self._get_36_flood()
 		self._get_zones()
 		self.overlapping_zones = {}
 		count = 0
 		for z in self.zones:
-			if z.intersects(self.flood):
+			try:
+				if z.intersects(self.flood):
 
-				overlapping, length = _get_overlapping_and_length(z, self.flood)
+					overlapping, length = _get_overlapping_and_length(z, self.flood)
 
-				self.overlapping_zones[count] = {
-					"id": count,
-					"overlapping zones": overlapping,
-					"flood intersect length": length
-				}
-				count += 1
+					# overlapping_list = []
+					# for a in overlapping:
+					# 	x, y = a.exterior.coords.xy
+					# 	overlapping_list.append(list(zip(x, y)))
+
+					# insertData = {
+					# 	"id": count,
+					# 	"overlapping zones": overlapping_list,
+					# 	"flood intersect length": length
+					# }
+					# overlapping_zones.insert_one(insertData)
+
+					self.overlapping_zones[count] = {
+						"id": count,
+						"overlapping zones": overlapping,
+						"flood intersect length": length
+					}
+
+					count += 1
+					print(count)
+			except:
+				pass
 
 	def _find_match_result(self):
 		self._generate_overlapping_zones()
@@ -88,7 +109,7 @@ class Flood_Cut:
 						zone = self.overlapping_zones[x]["overlapping zones"]
 					else:
 						zone = _sum_two_multipolygons(zone,\
-							   self.overlapping_zones[x]["overlapping zones"])
+								 self.overlapping_zones[x]["overlapping zones"])
 					intersect_length += self.overlapping_zones[x]["flood intersect length"]
 			total_length = _get_perimeter_of_multipolygon(zone)
 			wall_length = total_length - intersect_length
@@ -100,72 +121,64 @@ class Flood_Cut:
 		return answer
 
 
-	######## Static Methods #######
+######## Static Methods #######
 
-	@staticmethod
-	def _subtract_multipolygon_from_multipolygon(xs, ys) -> MultiPolygon:
-		outmulti = []
-		for p1 in xs:
-			for p2 in ys:
-				if p1.intersects(p2) == True:
-					nonoverlap = (p1.symmetric_difference(p2)).difference(p2)
-					outmulti.append(nonoverlap)
-				else:
-					outmulti.append(p1)
-		return MultiPolygon(outmulti)
-
-	@staticmethod
-	def _subtract_multipolygon_from_polygon(x, ys) -> MultiPolygon:
-		outmulti = []
-		for p in ys:
-			if x.intersects(p) == True:
-				nonoverlap = (x.symmetric_difference(p)).difference(p)
+def _subtract_multipolygon_from_multipolygon(xs, ys) -> MultiPolygon:
+	outmulti = []
+	for p1 in xs:
+		for p2 in ys:
+			if p1.intersects(p2) == True:
+				nonoverlap = (p1.symmetric_difference(p2)).difference(p2)
 				outmulti.append(nonoverlap)
 			else:
-				outmulti.append(x)
-		return MultiPolygon(outmulti)
+				outmulti.append(p1)
+	return MultiPolygon(outmulti)
 
-	@staticmethod
-	def _subtract_multipolygon(a, ys) -> MultiPolygon:
-		if type(a) == Polygon:
-			return _subtract_multipolygon_from_polygon(a, ys)
+def _subtract_multipolygon_from_polygon(x, ys) -> MultiPolygon:
+	outmulti = []
+	for p in ys:
+		if x.intersects(p) == True:
+			nonoverlap = (x.symmetric_difference(p)).difference(p)
+			outmulti.append(nonoverlap)
 		else:
-			return _subtract_multipolygon_from_multipolygon(a, ys)
+			outmulti.append(x)
+	return MultiPolygon(outmulti)
 
-	@staticmethod
-	def _sum_two_multipolygons(x, y):
-		return x.union(y)
+def _subtract_multipolygon(a, ys) -> MultiPolygon:
+	if type(a) == Polygon:
+		return _subtract_multipolygon_from_polygon(a, ys)
+	else:
+		return _subtract_multipolygon_from_multipolygon(a, ys)
 
-	@staticmethod
-	def _sum_list_multipolygons(xs):
-		if len(xs) == 0:
-			return None
-		outmulti = xs[0]
-		for x in xs[1:]:
-			outmulti = outmulti.union(x)
-		return outmulti
+def _sum_two_multipolygons(x, y):
+	return x.union(y)
 
-	@staticmethod
-	def _get_perimeter_of_multipolygon(x) -> float:
-		if x == None:
-			return 0.0
-		return x.length
+def _sum_list_multipolygons(xs):
+	if len(xs) == 0:
+		return None
+	outmulti = xs[0]
+	for x in xs[1:]:
+		outmulti = outmulti.union(x)
+	return outmulti
 
-	@staticmethod
-	def _get_overlapping_and_length(xs, ys):
-		part1 = _subtract_multipolygon(xs, ys)
-		part2 = _subtract_multipolygon(xs, part1)
-		perimeter = _get_perimeter_of_multipolygon(xs)
-		perimeter1 = _get_perimeter_of_multipolygon(part1)
-		perimeter2 = _get_perimeter_of_multipolygon(part2)
-		length = (perimeter1 + perimeter2 - perimeter) / 2
-		return (part2, length)
+def _get_perimeter_of_multipolygon(x) -> float:
+	if x == None:
+		return 0.0
+	return x.length
 
-	@staticmethod
-	def _get_area_of_multipolygon(x) -> float:
-		if x == None:
-			return 0.0
-		return x.area
+def _get_overlapping_and_length(xs, ys):
+	part1 = _subtract_multipolygon(xs, ys)
+	part2 = _subtract_multipolygon(xs, part1)
+	perimeter = _get_perimeter_of_multipolygon(xs)
+	perimeter1 = _get_perimeter_of_multipolygon(part1)
+	perimeter2 = _get_perimeter_of_multipolygon(part2)
+	length = (perimeter1 + perimeter2 - perimeter) / 2
+	return (part2, length)
+
+def _get_area_of_multipolygon(x) -> float:
+	if x == None:
+		return 0.0
+	return x.area
 
 if __name__ == '__main__':
 	f = Flood_Cut()
